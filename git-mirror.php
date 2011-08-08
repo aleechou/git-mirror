a0299b47ab83dbb57176312f252840889ecde88f
alee <zq@alee2002.com>
2011-08-08 20:24:49 +0800

    fixed a bug

--- git-mirror.php
+++ git-mirror.php
@@ -17,8 +17,14 @@ $temp_local_repo_dir = '/tmp/git-mirror' ;
 
 foreach($mirror_list as $sync_name => $remote_path_list)
 {
-	$local_tmp_path = $temp_local_repo_dir.'/'. md5(implode('>','$remote_path_list')) ;
+	$local_tmp_path = $temp_local_repo_dir.'/'. md5(implode('>',$remote_path_list)) ;
 
+	echo "\r\n\r\n----------------------------------------------------------\r\n" ;
+	echo "Sync ", $sync_name, "\r\n" ;
+	echo "repositories:\r\n" ;
+	echo implode("\r\n",$remote_path_list), "\r\n" ;
+	echo "local temp repository: ", $local_tmp_path, "\r\n" ;
+        
 	foreach($remote_path_list as $remote_path)
 	{
 		$remote_branch_list=`git ls-remote -h $remote_path | sed -n 's#^.*refs/heads/##p'`;
